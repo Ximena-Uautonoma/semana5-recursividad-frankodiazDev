40a7748ec4f71cf4a3e67996a349ee0b1cbb56d4
JotaEle245 <franquitoxxx2004@gmail.com>
2026-03-30 21:32:32 -0300

    Ejercicio 4 y 5

--- Semana5/ejercicio4.py
+++ Semana5/ejercicio4.py
@@ -4,8 +4,17 @@ Dado un número entero positivo N, contar cuántos números pares existen entre 
 """
 
 def contar_pares_ciclo(n):
-    pass
+    contador = 0
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            contador += 1
+    return contador
 
 
 def contar_pares_recursivo(n):
-    pass
+    if n == 0:
+        return 0
+    if n % 2 == 0: #es par
+        return 1 + contar_pares_recursivo(n - 1)
+    else:
+        return contar_pares_recursivo(n - 1)
--- Semana5/ejercicio5.py
+++ Semana5/ejercicio5.py
@@ -4,8 +4,14 @@ Calcular la potencia de una base elevada a un exponente entero positivo.
 """
 
 def potencia_ciclo(base, exponente):
-    pass
+    resultado = 1
+    for _ in range(exponente):
+        resultado *= base
+    return resultado
 
 
 def potencia_recursiva(base, exponente):
-    pass
+    if exponente == 0:
+        return 1
+    
+    return base * potencia_recursiva(base, exponente - 1)
